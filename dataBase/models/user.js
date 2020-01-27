24b8a926ab519de79d39a4523621d9f47ccc88c3
Oleh <olegflud5@gmail.com>
2020-01-27 15:05:36 +0200

    add dataBase

---
+++ dataBase/models/user.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize,DataTypes) =>{
+    const User = sequelize.define('User', {
+
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        surname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+    },{
+        tableName: 'users',
+        timestamps: false
+    });
+
+    return User;
+
+};
