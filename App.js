7d004949248b60e7e9b5afbf2fa0273c16fccb86
Oleh <olegflud5@gmail.com>
2020-01-27 13:45:16 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const path = require('path');
+const app = express();
+const http = require('http').createServer(app);
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'static')));
+app.use(express.urlencoded({extended:true}));
+
+
+app.set('views',path.join(__dirname,'static'));
+
+
+http.listen(5000,()=>{
+    console.log('5000');
+});
+
