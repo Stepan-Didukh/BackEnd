e816c868c8c641311b9517807cd1870f1cb866ee
Oleh <olegflud5@gmail.com>
2020-01-28 17:53:43 +0200

    add some functionals createUser

--- dataBase/models/User.js
+++ dataBase/models/User.js
@@ -22,12 +22,27 @@ module.exports = (sequelize,DataTypes) =>{
         password: {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        role_id:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        status_id:{
+            type: DataTypes.INTEGER,
+            allowNull: false
         }
 
     },{
         tableName: 'users',
         timestamps: false
     });
+
+    const Role = sequelize.import('./Role.js');
+    const UserStatus = sequelize.import('./UserStatus.js');
+
+    User.belongsTo(Role,{foreignKey:'role_id'});
+    User.belongsTo(UserStatus,{foreignKey:'status_id'});
+
 
     return User;
 
